114e8da98e34a6ba9fdcc44afc3cd7093638609f
Vladimir Dobriakov <vladimir@geekq.net>
2008-11-25 15:29:30 +0100

    more detailed readme instead of trying to run the generator in the install.rb

--- install.rb
+++ install.rb
@@ -1,3 +1,1 @@
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
-gg = GettextHacksGenerator.new([[], {:collision=>:ask, :quiet=>false, :generator=>"gettext_hacks", :command=>:create}])
-gg.command(:create).invoke!
